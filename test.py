ef58aa7f19c5223eaf018d6c9dcb3966bb5b235a
folke99 <folkefd@gmail.com>
2018-09-19 02:41:51 +0200

    Commit #1?

---
+++ test.py
@@ -0,0 +1,6 @@
+def Folke(sport, food, friend):
+    print("I like playing {0} with {2} then eating some {1}".format(sport, food, friend))
+Folke("golf", "Sushi", "Nelly")
+Folke("1", "2", "3")
+print("why")
+print("commit to tight place?")
